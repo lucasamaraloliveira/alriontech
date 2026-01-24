4c28489368f2a03bd3ac3a29374a3cedb2ce5eca
Lucas Amaral Oliveira da Silva <37918350+lucasamaraloliveira@users.noreply.github.com>
2026-01-24 20:29:41 -0300

    Feat: instalado o vite-plugin-compression
    Agora, quando o site for construído para produção (o build final), os arquivos de JavaScript e CSS serão automaticamente comprimidos (Gzip), o que reduz drasticamente o tamanho do download inicial e resolve o problema de latência que aparecia no relatório.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
@@ -13,6 +14,7 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       tailwindcss(),
+      viteCompression()
     ],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
